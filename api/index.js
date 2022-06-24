2625bc7e2f93dd6fc20fc71694670791970aedf7
chalexan <cherednychenko.alexandr@gmail.com>
2022-06-24 15:42:40 +0300

    fix-port-kill

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const kill = require('kill-port')
 const app = express()
 const port = 5000
+
+kill(port, 'tcp');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
